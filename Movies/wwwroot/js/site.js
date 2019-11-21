05d60829276eddebfc3e8c743104a55c9b50a023
jhpleimann <j.plmnn@gmail.com>
2019-11-21 15:24:20 -0600

    AJAX UPDATE!

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var form = document.getElementById("search-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("searching...");
+
+    var inputs = document.getElementsByName("input");
+
+    var formData = new FormData();
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs.type == 'checkbox') {
+            if (input.checked) { formData.append(inputs.name, inputs.value); }
+        }
+        else {
+            formData.set(inputs.name, inputs.value);
+        }
+        if (inputs[i].name == "__RequestVerificationToken") {
+            token = inputs[i].value;
+        }
+}
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', '/', true);
+    xhr.setRequestHeader("RequestVerificationToken", token);
+    xhr.onreadystatechange();
+    xhr.send;
+
+
+});
